67e6c3c8ec91e64c96ddb55da3072cc6b086000c
Cat Hallam <catherinehallam@mutumojaa.com>
2018-06-17 17:42:16 +0100

    Add Pet Age

--- src/pet.js
+++ src/pet.js
@@ -1,7 +1,21 @@
-function Pet(name) {
+  //Option 1
+  function Pet(name) {
     this.name = name;
+    this.age = 0;
   }
   
+  Pet.prototype.growUp = function() {
+    this.age += 1;
+  };
 
+//Option 2
+//function Pet(name) {
+   // this.name = name;
+    //this.age = 0;
+    // this.growUp = function() {
+    //   this.age += 1;
+    // };
+ // }
+  
 
 module.exports = Pet;
