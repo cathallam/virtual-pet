255f4d6e2bbc1500a8a600d849cb079c5e160733
Cat Hallam <catherinehallam@mutumojaa.com>
2018-06-18 20:13:40 +0100

    Virtual Pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -22,5 +22,38 @@ describe('constructor', () => {
     });
   });
   
-  
-  
+  describe('constructor', () => {
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+      it('increases the hunger by 5', () => {
+          const pet = new Pet('Fido');
+          pet.growUp();
+          expect(pet.hunger).toEqual(5);
+      });
+    });describe('constructor', () => {
+    it('has an initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+  });  
+
+    describe('growUp', () => {
+        it('increments the hunger by 5', () => {
+          const pet = new Pet('Fido');
+          pet.growUp();
+          expect(pet.hunger).toEqual(5);
+        });
+      });
+    
+      describe('growUp', () => {
+        it('decrements the fitness by 3', () => {
+          const pet = new Pet('Fido');
+          pet.growUp();
+          expect(pet.fitness).toEqual(7);
+        });
+      });
--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,22 @@
   //Option 1
+  
   function Pet(name) {
+    //this.name = name, this refers to the object returned from by calling new Pet(), 
+   //so this.name refers to the name property of that object.
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
   }
   
+  // OBJECTS PROTOTYPE
   Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
   };
 
-//Option 2
+//OPTION 2 INSTANCE METHOD
 //function Pet(name) {
    // this.name = name;
     //this.age = 0;
